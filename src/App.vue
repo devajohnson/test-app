930d72f5469ae05c46d9338655608ed7395a1028
devajohnson <93877296+devajohnson@users.noreply.github.com>
2021-11-15 14:10:19 +0530

    test project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Getelement />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Getelement from './components/Getelement.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Getelement
   }
 }
 </script>
---
+++ src/components/Getelement.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="ml-3">
+        <!-- <p>{{msg}}</p> -->
+        <table class="gettable">
+            <thead>
+                <tr>
+                    <th scope="col">SI</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">user Id</th>
+                    <th scope="col">body</th>
+
+                </tr>
+            </thead>
+            <tbody v-for="key in data" :key="key.id">
+                <tr>
+                <td>{{key.id}}</td>
+                <td>{{key.title}}</td>
+                <td>{{key.userId}}</td>
+                <td>{{key.body}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</template>
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: 'Getelement',
+ 
+    data(){
+        return{
+            data:[]
+        }
+    },
+    created(){
+        this.apicall()
+    },
+    methods:{
+        apicall(){
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((responce)=>
+            this.data = responce.data)
+            // console.log(this.data)
+        }
+    }
+}
+</script>
