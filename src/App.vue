c8ffd5003f9668a05d1a5b9bdcc6fff70562ee65
devajohnson <93877296+devajohnson@users.noreply.github.com>
2021-11-15 13:44:06 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <Getelement />
-  <Hooks />
+  <!-- <Getelement /> -->
+  <!-- <Hooks /> -->
   <Composite />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Getelement from './components/Getelement.vue'
-import Hooks from './components/Hooks.vue'
+// // import Getelement from './components/Getelement.vue'
+// // import Hooks from './components/Hooks.vue'
 import Composite from'./components/Composite.vue'
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    Getelement,
-    Hooks,
+    // Getelement,
+    // Hooks,
     Composite,
   },
   data(){
